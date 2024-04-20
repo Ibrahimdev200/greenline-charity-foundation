1ae72b7111347641fdc300e37a1305c4795a590b
quadri damilare <ibrahimdev20@gmail.com>
2024-04-20 23:35:55 +0100

    secont commit

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function increaseAmount() {
+
+  function increaseAmount() {
     var amountInput = document.getElementById('amount');
     var currentValue = parseInt(amountInput.value);
     if (!isNaN(currentValue)) {
@@ -17,12 +18,31 @@ function increaseAmount() {
   document.addEventListener('DOMContentLoaded', function() {
     const menuIcon = document.querySelector('.menu-icon');
     const menu = document.querySelector('.menu');
+    const menuItems = document.querySelectorAll('.menu a');
+
   
     menuIcon.addEventListener('click', function() {
       menuIcon.classList.toggle('active');
       menu.classList.toggle('active');
-    });
-  });
 
+
+      menuItems.forEach(item => {
+        item.addEventListener('click', function() {
+          // Hide the menu
+          const menu = document.querySelector('.menu');
+          menu.classList.remove('active');
+    
+          // Hide the menu icon if it's open
+          const menuIcon = document.querySelector('.menu-icon');
+          menuIcon.classList.remove('active');
+        });
+      });
+    });
+    });
   
-  
+
+
+
+
+
+
